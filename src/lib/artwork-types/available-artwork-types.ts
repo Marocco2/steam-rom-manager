206f443d39186af5eef7beb1551ea56ce4470087
Marocco2 <marocco2@live.it>
2026-07-22 10:56:47 +0200

    Reorder artwork file names in steamArtworkDict by quality
    SRM will pick better quality artworks first now

--- src/lib/artwork-types/available-artwork-types.ts
+++ src/lib/artwork-types/available-artwork-types.ts
@@ -41,11 +41,11 @@ export const artworkSingDict: Record<ArtworkType, string> = {
 };
 
 export const steamArtworkDict: Record<ArtworkType, string[]> = {
-  tall: ["library_600x900.jpg", "library_600x900_2x.jpg"],
-  long: ["header.jpg", "capsule_616x353.jpg", "capsule_231x87.jpg"],
+  tall: ["library_600x900_2x.jpg", "library_600x900.jpg"],
+  long: ["capsule_616x353.jpg", "header.jpg", "capsule_231x87.jpg"],
   hero: [
-    "library_hero.jpg",
     "library_hero_2x.jpg",
+    "library_hero.jpg",
     "page_bg_generated_v6b.jpg",
   ],
   logo: ["logo.png", "logo_2x.png"],
